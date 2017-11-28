09121ffabcdc3a5dacb571222e00aa9b6958dbf9
steinum <richi.anden@centrum.sk>
2017-11-28 16:58:23 +0100

    update

--- music/views.py
+++ music/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('<h1> music app web </h1>')
+
+def detail(request, album_id):
+    return HttpResponse('<h2> Details for album id:' + str(album_id) + '</h2>')
