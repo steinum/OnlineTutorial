cb4c99223f5502a20d6d95d0ff36bec8cedbd191
steinum <richi.anden@centrum.sk>
2017-12-29 14:17:59 +0100

    update

--- music/views.py
+++ music/views.py
@@ -1,7 +1,25 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
+
 
 def index(request):
-    return HttpResponse('<h1> music app web </h1>')
+    all_albums = Album.objects.all()
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 def detail(request, album_id):
-    return HttpResponse('<h2> Details for album id:' + str(album_id) + '</h2>')
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album': album})
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message': "zly vyber",
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
